8de6a9cb240261a0666d0717916c7a7a6118f43b
nizzle <nizom7812@gmail.com>
2024-09-29 18:49:49 +1000

    initial commit

---
+++ example.py
@@ -0,0 +1,52 @@
+from oauth3 import bls, utils, encrypt, decrypt
+
+seed: bytes = bytes([0,  50, 6,  244, 24,  199, 1,  25,  52,  88,  192,
+                        19, 18, 12, 89,  6,   220, 18, 102, 58,  209, 82,
+                        12, 62, 89, 110, 182, 9,   44, 20,  254, 22])
+
+# Generate private key and public key
+sk, pk = bls.key_gen(seed)
+
+print('sk',sk)
+print('pk',pk)
+
+# Export and import secret key
+sk = utils.export_sk(sk) # only when storing, default type is 'hex'
+print('sk1',sk)
+sk = utils.import_sk(sk) # only when loading, default type is 'hex'
+
+print('sk1',sk)
+
+# Export and import public key
+pk = utils.export_pk(pk) # only when storing, default type is 'hex'
+print('pk1',pk)
+pk = utils.import_pk(pk) # only when loading, default type is 'hex'
+
+print('pk1',pk)
+
+# Tag to be signed, it's signature is required for decrypting the message
+tag: str = 'tag'
+# Message to be encrypted
+message: str = 'Hello, world!'
+
+# Sign the tag
+signature = bls.sign(sk, tag)
+
+# Export and import signature
+signature = utils.export_sig(signature) # only when storing, default type is 'hex'
+signature = utils.import_sig(signature) # only when loading, default type is 'hex'
+
+# Verify the signature
+ok: bool = bls.verify(pk, tag, signature)
+assert ok
+print("Signature verified")
+
+# Encrypt the message
+ciphertext: str = encrypt(pk, tag, message)
+print("Ciphertext:\n" + ciphertext)
+
+# Decrypt the message
+decrypted_message: str = decrypt(signature, ciphertext)
+
+assert message == decrypted_message
+print("Decrypted message\n" + decrypted_message)
---
+++ oauth3/__init__.py
@@ -0,0 +1,5 @@
+from . import bls
+from . import utils
+from . import groups
+from .oauth3 import enc as encrypt
+from .oauth3 import dec as decrypt
